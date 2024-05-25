41e995aafac865333a62a653d3ea13b43e924adf
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2024-05-25 16:48:34 +0530

    Time: 18 ms (93.88%) | Memory: 38.5 MB (35.28%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -5,15 +5,17 @@ public:
         int count1,count2;
         vector<int> arr;
         count1=0;
+        count2=0;
         for(int i=0;i<n;i++){
             if(nums[i]==1)
             count1++;
-           else 
-           { arr.push_back(count1);
-            count1=0;}
+           else {
+            count2=max(count1,count2);
+            count1=0;
+           }
         }
-        arr.push_back(count1);
-int max = *max_element(arr.begin(),arr.end());
-        return max;
+        count2=max(count1,count2);
+        
+        return count2;
     }
 };
