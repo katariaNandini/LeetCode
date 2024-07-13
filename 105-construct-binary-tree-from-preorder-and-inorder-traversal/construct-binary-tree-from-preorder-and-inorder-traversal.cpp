08c67b8e86e0e2281b9f8ae1c5ae79d7bfa2b943
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2024-07-14 00:08:39 +0530

    Time: 13 ms (53.02%) | Memory: 27.6 MB (13.70%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,56 +10,26 @@
  * };
  */
 class Solution {
-    public:
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder){
-        // Create a map to store indices
-        // of elements in the inorder traversal
-        map<int, int> inMap;
-        
-        // Populate the map with indices
-        // of elements in the inorder traversal
-        for(int i = 0; i < inorder.size(); i++){
-            inMap[inorder[i]] = i;
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        //int n=inorder.size();
+        map<int,int> inmap;
+        for(int i=0;i<inorder.size();i++){
+inmap[inorder[i]]=i;
         }
-        
-        // Call the private helper function
-        // to recursively build the tree
-        TreeNode* root = buildTree(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, inMap);
-        
+        TreeNode* root=buildTree(preorder,0,preorder.size()-1,
+        inorder,0,inorder.size()-1,inmap);
         return root;
     }
-
-
-    // Recursive helper function to build the tree
-    TreeNode* buildTree(vector<int>& preorder, int preStart, int preEnd, 
-            vector<int>& inorder, int inStart, int inEnd, map<int, int>& inMap){
-                // Base case: If the start indices 
-                // exceed the end indices, return NULL
-                if(preStart > preEnd || inStart > inEnd){
-                    return NULL;
-                }
-                
-                // Create a new TreeNode with value
-                // at the current preorder index
-                TreeNode* root = new TreeNode(preorder[preStart]);
-                
-                // Find the index of the current root
-                // value in the inorder traversal
-                int inRoot = inMap[root->val];
-                
-                // Calculate the number of
-                // elements in the left subtree
-                int numsLeft = inRoot - inStart;
-                
-                // Recursively build the left subtree
-                root->left = buildTree(preorder, preStart + 1, preStart + numsLeft, 
-                                inorder, inStart, inRoot - 1, inMap);
-                
-                // Recursively build the right subtree
-                root->right = buildTree(preorder, preStart + numsLeft + 1, preEnd, 
-                                inorder, inRoot + 1, inEnd, inMap);
-                
-                // Return the current root node
-                return root;
-            }
+    TreeNode * buildTree(vector<int>& preorder,int prestart,
+    int preend, vector<int>& inorder,int instart,int inend,map<int ,int >&inmap){
+        if(prestart>preend||instart>inend) return NULL;
+        TreeNode* root=new TreeNode(preorder[prestart]);
+        int inroot=inmap[root->val];
+        int numleft=inroot-instart;
+        root->left=buildTree(preorder,prestart+1,prestart+numleft,inorder,instart,
+        inroot-1,inmap);
+        root->right=buildTree(preorder,prestart+numleft+1,preend,inorder,inroot+1,inend,inmap);
+        return root;
+    }
 };
