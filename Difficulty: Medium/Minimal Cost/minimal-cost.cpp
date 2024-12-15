036cc641c378c43b210ee82cd76220c3bdf9940d
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2024-12-15 21:01:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) {
+        // Code here
+        int n=arr.size();
+        vector<int> dp(n+1,0);
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            vector<int> findmin;
+            for(int j=1;j<=k;j++){
+                int ls=INT_MAX;
+                if(i-j>=0) ls=dp[i-j]+abs(arr[i]-arr[i-j]);
+                findmin.push_back(ls);
+                
+            }
+            dp[i]=*min_element(findmin.begin(),findmin.end());
+            
+            // int ls=dp[i-1]+abs(height[i]-height[i-1]);
+            // int rs=INT_MAX;
+            // if(i>1)
+            // rs=dp[i-2]+abs(height[i]-height[i-2]);
+            // dp[i]=min(rs,ls);
+        }
+        return dp[n-1];
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.minimizeCost(k, arr);
+        cout << res << endl;
+        cout << "~" << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
