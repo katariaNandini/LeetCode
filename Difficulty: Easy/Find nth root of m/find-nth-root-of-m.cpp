9675f8091ef7b7c280e8aedcb43a237decf176da
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2024-12-26 23:37:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int nthRoot(int n, int m) {
+        for(int i=0;i<=m;i++){
+            
+            if(pow(i,n)==m) return i;
+            if(pow(i,n)>m) return -1;
+          
+        }
+          return -1;
+        // Code here.
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        Solution ob;
+        int ans = ob.nthRoot(n, m);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
