dc1d833fa3b143376692a4660d441e883e99f203
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2024-12-28 23:09:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -7,36 +7,37 @@ using namespace std;
 class Solution {
   public:
     int kthElement(vector<int>& a, vector<int>& b, int k) {
-          int n1 = a.size();
-        int n2 = b.size();
-
-        // If k is larger than the combined length, return -1 (invalid case)
-        if (k > n1 + n2) return -1;
-        
-        // Binary search on the smaller array
+        int n1=a.size();
+        int n2=b.size();
+                if (k > n1 + n2) return -1;
         if (n1 > n2) return kthElement(b, a, k);
 
-        int low = max(0, k - n2), high = min(k, n1);
-        
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = k - mid1;
-            
-            int l1 = (mid1 > 0) ? a[mid1 - 1] : INT_MIN;
-            int l2 = (mid2 > 0) ? b[mid2 - 1] : INT_MIN;
-            int r1 = (mid1 < n1) ? a[mid1] : INT_MAX;
-            int r2 = (mid2 < n2) ? b[mid2] : INT_MAX;
-            
-            if (l1 <= r2 && l2 <= r1) {
-                return max(l1, l2);
-            } else if (l1 > r2) {
-                high = mid1 - 1;
-            } else {
-                low = mid1 + 1;
+       // int half=k/2;
+     int low = max(0, k - n2);
+int high = min(k, n1);
+        //int totalh=(n1+n2+1)/2;
+        //int ansindex=-1;
+        while(low<=high){
+            int mid1=(high+low)/2;
+            int mid2=k-mid1;
+            int l1=INT_MIN;
+            int l2=INT_MIN;
+            int r1=INT_MAX;
+            int r2=INT_MAX;
+
+            if(mid1 >0) l1=a[mid1-1];
+            if(mid2 >0) l2=b[mid2-1];
+            if(mid1<n1) r1=a[mid1];
+            if(mid2<n2) r2=b[mid2];
+            if(l1<=r2&&l2<=r1) {
+            return max(l1,l2);
+               // return (double(max(l1,l2)+min(r1,r2)))/2.0;
             }
+           else  if(l1>r2) high=mid1-1;
+             else low=mid1+1;
         }
-        
-        return -1;  // Return -1 if no kth element is found (shouldn't happen)
+return -1;
+        // code here
     }
 };
 
