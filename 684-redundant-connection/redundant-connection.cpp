54ef0f7be3e36e04d299a447f13a9db356bd53c7
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-01-29 18:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (69.21%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,25 +1,42 @@
+class DSU{
+    public:
+    vector<int>parent;
+    vector<int> rank;
+    DSU(int n){
+        parent.resize(n+1);
+        rank.resize(n+1);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+    int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
+    }
+    void Union(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+        if(x_parent==y_parent)return ;
+         if(rank[x_parent]>rank[y_parent]) parent[y_parent]=x_parent;
+         else if(rank[x_parent]<rank[y_parent]) parent[x_parent]=y_parent;
+         else {
+parent[y_parent]=x_parent;
+rank[x_parent]++;
+         }
+    }
+};
+
 class Solution {
 public:
-bool dfs( unordered_map<int,vector<int>> &adj,int u,int v,vector<bool> &visited){
-    visited[u]=true;
-    if(u==v) return true;
-    for(int &nbr:adj[u]){
-        if(visited[nbr])continue;
-        if(dfs(adj,nbr,v,visited))return true;
-    }
-    return false;
-}
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        unordered_map<int,vector<int>> adj;
-        for(int i=0;i<n;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            vector<bool> visted(n,false);
-            if(adj.find(u)!=adj.end()&&adj.find(v)!=adj.end()&&dfs(adj,u,v,visted))
-            return edges[i];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        DSU dsu(n);
+        for(auto&edges:edges){
+            int u=edges[0];
+            int v=edges[1];
+            if(dsu.find(u)==dsu.find(v)) return edges;
+dsu.Union(u,v);
         }
         return {};
     }
