81a978e281d50ee4882ff6a6884c0ad65b8303d6
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-01-29 16:55:29 +0530

    Time: 6 ms (19.91%) | Memory: 14.5 MB (15.94%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool dfs( unordered_map<int,vector<int>> &adj,int u,int v,vector<bool> &visited){
+    visited[u]=true;
+    if(u==v) return true;
+    for(int &nbr:adj[u]){
+        if(visited[nbr])continue;
+        if(dfs(adj,nbr,v,visited))return true;
+    }
+    return false;
+}
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        unordered_map<int,vector<int>> adj;
+        for(int i=0;i<n;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            vector<bool> visted(n,false);
+            if(adj.find(u)!=adj.end()&&adj.find(v)!=adj.end()&&dfs(adj,u,v,visted))
+            return edges[i];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        return {};
+    }
+};
