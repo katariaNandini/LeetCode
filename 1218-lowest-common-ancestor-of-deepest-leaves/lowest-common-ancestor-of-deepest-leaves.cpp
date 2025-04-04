cc525d4c2bd3166d4304befd12e67d6ef5f78957
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-04-05 02:42:26 +0530

    Time: 3 ms (19.45%) | Memory: 23.2 MB (15.93%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+unordered_map<int,int> mp;
+int maxlength=0;
+void depth(TreeNode * root,int d){
+    if(!root) return ;
+    maxlength=max(d,maxlength);
+    mp[root->val]=d;
+    depth(root->left,d+1);
+    depth(root->right,d+1);
+}
+TreeNode * LCA(TreeNode * root){
+    if(root==NULL||mp[root->val]==maxlength) return root;
+    TreeNode * l=LCA(root->left);
+    TreeNode * r=LCA(root->right);
+    if(l==NULL) return r;
+    else if(r==NULL) return l;
+else{
+    return root;
+}
+}
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        depth(root,0);
+        return LCA(root);
+    }
+};
