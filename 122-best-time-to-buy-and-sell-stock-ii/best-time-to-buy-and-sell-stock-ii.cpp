2bef7f3742ed3060492a697d12b13c46a7dfb331
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-05-19 01:49:37 +0530

    Time: 3 ms (32.55%) | Memory: 19.8 MB (22.31%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -14,21 +14,22 @@ if(dp[ind][buy]!=-1) return dp[ind][buy];
         int n=prices.size();
 
         vector<vector<int>> dp(n,vector<int> (2,-1));
-
+vector<int> prev(2,0),cur(2,0);
 //         if(ind==n&&buy) return 0;
 // if(ind==n&&!buy) return prices[ind];
-dp[n-1][1]=0;
-dp[n-1][0]=prices[n-1];
+cur[1]=0;
+cur[0]=prices[n-1];
         for(int ind=n-2;ind>=0;ind--){
             for(int buy=1;buy>=0;buy--){
                  if(buy){
-         dp[ind][buy]=max((-prices[ind]+ dp[ind+1][0] ),(0+ dp[ind+1][1] ));
+         prev[buy]=max((-prices[ind]+ cur[0] ),(0+ cur[1] ));
     }
-    else  dp[ind][buy]=max((prices[ind]+ dp[ind+1][1] ),(0+dp[ind+1][0] ));
+    else prev[buy]=max((prices[ind]+ cur[1] ),(0+cur[0] ));
 
             }
+            cur=prev;
         }
-        return dp[0][1];
+        return prev[1];
         
     }
 };
