bbc117066c6069467630ad77bdf2f154b1d34425
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-05-23 00:01:00 +0530

    Time: 19 ms (92.59%) | Memory: 74.9 MB (76.49%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -8,15 +8,24 @@ public:
          int count=0;
          int maxi=0;
          while(j<n){
-            mp[fruits[j]]++;
+            mp[fruits[j]]=j;
             if(mp.size()>2){
-                mp[fruits[i]]--;
-                  if(mp[fruits[i]]==0){
-                    mp.erase(fruits[i]);
-                  } 
-                i++;
+                            int minIndex = INT_MAX;
+                int fruitToRemove = -1;
+                for (auto& [fruit, index] : mp) {
+                    if (index < minIndex) {
+                        minIndex = index;
+                        fruitToRemove = fruit;
+                    }
+                }
 
-            }
+                mp.erase(fruitToRemove);
+                i = minIndex + 1;
+
+                  } 
+              
+
+            
           count=j-i+1;
           maxi=max(count,maxi);
           j++;
