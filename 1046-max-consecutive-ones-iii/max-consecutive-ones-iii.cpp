1ab62cd7266055262abdcc51afc2c2ff90f12eb7
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-05-22 16:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 59.2 MB (58.54%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -8,23 +8,31 @@ public:
         int count=0;
         while(r<n){
             
-            if(nums[r]==1){
+            // if(nums[r]==1){
            
-            r++;
-            }
-           else  if(nums[r]==0&&z<k){
+            // r++;         }
+             if(nums[r]==0){
                 z++;
-                r++;
+                // r++;
             }
-            else{
-                while(nums[l]!=0){
-                    l++;
+            // else{
+            //     while(nums[l]!=0){
+            //         l++;
+            //     }
+            if(z>k){
+                if(nums[l]==0){
+                    z--;
                 }
             l++;
-                z--;
             }
-            count=r-l;
+               if(z<=k){
+ count=r-l+1;
             maxi=max(count,maxi);
+
+               }
+               r++;
+            
+           
         }
         return maxi;
     }
