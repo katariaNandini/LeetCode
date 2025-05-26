54ba8450ab2f3113ba5fcd2e6adb56328a13be0f
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-05-26 23:36:50 +0530

    Time: 88 ms (41.38%) | Memory: 164.8 MB (22.57%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int maxi = INT_MIN, start = -1;
-    void oddEven(int i, int j, string &s) {
-        while(i>=0 && j<s.size() && s[i]==s[j]) {
-            if(maxi<j-i+1) {
-                maxi = j-i+1;
-                start = i;
+// see the prev submistted solution you will understand better 
+int maxi=INT_MIN;
+int start=-1;
+    void oe(int i,int j,string s,int n){
+        while(i>=0&&j<n&&s[i]==s[j]){
+            int len=j-i+1;
+            if(len>maxi){
+                maxi=len;
+                start=i;
+
             }
             i--;
             j++;
         }
     }
-
     string longestPalindrome(string s) {
-        for(int i=0;i<s.size();i++) {
-            oddEven(i,i,s);
-            oddEven(i,i+1,s);
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            oe(i,i,s,n);
+            oe(i,i+1,s,n);
         }
-
-        return s.substr(start,maxi);
+return s.substr(start,maxi);
     }
 };
