a58f93bbbbcff2a9174da218999d8d8b14ac8ac6
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-05-27 23:30:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (13.68%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        if(haystack==needle) return 0;
-        int n=haystack.length();
-        int m=needle.length();
-        for(int i=0;i<=n-m;i++){
-            for(int j=0;j<m;j++){
-                if(haystack[i+j]!=needle[j]) break;
-                if(j==m-1) return i;
+    int strStr(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        int i=1;
+        int len=0;
+        vector<int>lps(m);
+        lps[0]=0;
+        while(i<m){
+            if(t[i]==t[len]){
+                len++;
+                lps[i]=len;
+                i++;
+                
+            }
+            else {
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else {lps[i]=0;
+                i++;}
             }
         }
-return -1;
-
+        i=0;
+        int j=0;
+        while(i<n){
+            if(s[i]==t[j]){
+                i++;
+                j++;
             }
+            if(j==m) return i-j;
+    else if(s[i]!=t[j]){
+        if(j!=0) j=lps[j-1];
+        else i++;
+    }
+    
+        }
+        return -1;
+    }
 };
