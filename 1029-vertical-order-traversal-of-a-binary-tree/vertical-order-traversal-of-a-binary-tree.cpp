1e1c2da91f3e54b55485a97f68215115ef043acd
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-05-29 18:08:39 +0530

    Time: 3 ms (42.13%) | Memory: 15.2 MB (98.99%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,28 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void traversal(TreeNode* root, vector<tuple<int, int, int>>& res, int row, int col) {
-        if (root == NULL) return;
-        res.push_back({col, row, root->val});
-        traversal(root->left, res, row + 1, col - 1);
-        traversal(root->right, res, row + 1, col + 1);
+void traversal(TreeNode* root,vector<tuple<int,int,int>>&res,int level,int col){
+    if(root==NULL) return ;
+    res.push_back({col,level, root->val});
+
+    if(root->left){
+        // res.push_back({level-1,root->left->val});
+    traversal(root->left,res,level-1,col+1);
     }
-
+    if(root->right){
+        // res.push_back({level+1,root->right->val});
+    traversal(root->right,res,level+1,col+1);
+    }
+}
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<tuple<int, int, int>> res;
-        traversal(root, res, 0, 0); // row = 0, col = 0
-
-        sort(res.begin(), res.end()); // sorted by col, then row, then val
-
-        map<int, vector<int>> group;
-        for (auto [col, row, val] : res) {
-            group[col].push_back(val);
-        }
-
+        vector<tuple<int,int,int>> res;
+        traversal(root,res,0,0);
+        sort(res.begin(), res.end());
         vector<vector<int>> ans;
-        for (auto& [col, vals] : group) {
-            ans.push_back(vals);
-        }
+        map<int,vector<int>> group;
 
-        return ans;
+        for( auto [col, row, val]:res){
+            group[row].push_back(val);
+        }
+        for(auto it:group){
+            ans.push_back(it.second);
+        }
+        //         int level=it.first;
+        //         int val=it.second;
+        //         vector<int> temp;
+        //         while(it.first==level){
+        //                 temp.push_back(val);
+                        
+        //         }
+
+
+        // }
+return ans;
     }
 };
