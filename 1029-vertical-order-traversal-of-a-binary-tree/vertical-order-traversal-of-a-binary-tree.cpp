9714d208fcaaff3a0362ac10d45563c627b4fdf4
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-05-29 18:04:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (97.78%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,57 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>>nodes;
-        queue<pair<TreeNode*,pair<int,int>>>todo;
-todo.push({root,{0,0}});
-while(!todo.empty()){
-    auto p=todo.front();
-    todo.pop();
-    int x=p.second.first;
-    int y=p.second.second;
-                TreeNode* temp = p.first;
-
-    nodes[x][y].insert(temp->val);
-    if(temp->left){
-todo.push({
-                    temp->left,
-                    {
-                        // Move left in
-                        // terms of vertical
-                        x-1, 
-                        // Move down in
-                        // terms of level
-                        y+1  
-                    }
-                });    }
-    if(temp->right){
-        todo.push({{temp->right},{x+1,y+1}});
+    void traversal(TreeNode* root, vector<tuple<int, int, int>>& res, int row, int col) {
+        if (root == NULL) return;
+        res.push_back({col, row, root->val});
+        traversal(root->left, res, row + 1, col - 1);
+        traversal(root->right, res, row + 1, col + 1);
     }
-    
-}
-vector<vector<int>> ans;
-        for(auto p: nodes){
-            vector<int> col;
-            for(auto q: p.second){
-                // Insert node values
-                // into the column vector
-                col.insert(col.end(), q.second.begin(), q.second.end());
-            }
-            // Add the column vector
-            // to the final result
-            ans.push_back(col);
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<tuple<int, int, int>> res;
+        traversal(root, res, 0, 0); // row = 0, col = 0
+
+        sort(res.begin(), res.end()); // sorted by col, then row, then val
+
+        map<int, vector<int>> group;
+        for (auto [col, row, val] : res) {
+            group[col].push_back(val);
         }
+
+        vector<vector<int>> ans;
+        for (auto& [col, vals] : group) {
+            ans.push_back(vals);
+        }
+
         return ans;
     }
 };
