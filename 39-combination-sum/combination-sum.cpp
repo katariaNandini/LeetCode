8dc7145d39fe0c72ea18452a82adf296130b6d42
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-10 01:11:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (73.21%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+void f(vector<int>& c,int t,int i,int n,vector<int> &ds,  vector<vector<int>> &ans){
+    // if(i==n) {
+        if(t==0){ ans.push_back(ds);
+        return ;
+    }
+    if (t < 0 || i == n) return;
+    ds.push_back(c[i]);
+    f(c,t-c[i],i,n,ds,ans);
+    ds.pop_back();
+    f(c,t,i+1,n,ds,ans);
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> ds;
+        int i=0,n=candidates.size();
+        vector<vector<int>> ans;
+        f(candidates,target,i,n,ds,ans);
+        return ans;
+    }
+};
