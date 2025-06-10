da1b52e95606ca865dc16b57f8a138e0e9fd15d8
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-10 15:03:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.cpp
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -1,66 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-  void sub(vector<int>& arr,int n,int ind,int sum,vector<int>&ans){
-      if(ind==n) {ans.push_back(sum);
-          return;
-      }
-      sub(arr,n,ind+1,sum+arr[ind],ans);
-      sub(arr,n,ind+1,sum,ans);
+  void f(vector<int>& arr,vector<int>&sum,int ind,int s){
+      if(ind==arr.size()){sum.push_back(s);
+      return ;}
+    //   take
+    f(arr,sum,ind+1,s+arr[ind]);
+     f(arr,sum,ind+1,s);
+    
       
       
   }
     vector<int> subsetSums(vector<int>& arr) {
-        int n=arr.size();
-        vector<int> ans;
-        sub(arr,n,0,0,ans);
-            sort(ans.begin(), ans.end());
-
-        return ans;
-        
         // code here
+        vector<int> sum;
+        f(arr,sum,0,0);
+        // vector<int> ans(sum.begin(),sum.end());
+        return sum;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t; // Number of test cases
-    cin >> t;
-    cin.ignore(); // Ignore the newline after the integer input
-
-    while (t--) {
-        vector<int> inputArray;
-        string inputLine;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, inputLine);
-        stringstream inputStream(inputLine);
-        int num;
-        while (inputStream >> num) {
-            inputArray.push_back(num); // Read the array elements from input string
-        }
-
-        Solution solutionObject;
-        vector<int> result = solutionObject.subsetSums(inputArray);
-        sort(result.begin(), result.end());
-
-        for (int i = 0; i < result.size(); i++) {
-            if (i != 0)
-                cout << " ";
-            cout << result[i];
-        }
-        cout
-            << endl
-            << "~\n"; // Print results in list format with new line after each test case
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
