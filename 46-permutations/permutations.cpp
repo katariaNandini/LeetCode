b2414246a942fb27f18ed9e4f061e0981328a4dd
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-10 16:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (87.64%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-void f(vector<int> &nums,vector<int> &ds,vector<int>&mp,vector<vector<int>> &ans){
-    if(ds.size()==nums.size()) {ans.push_back(ds);
-    return ;}
-    // if(ind=nums.size()) return ;
-    for(int i=0;i<nums.size();i++){
-        if(mp[i]==0){
-            mp[i]=1;
-            ds.push_back(nums[i]);
-            f(nums,ds,mp,ans);
-            mp[i]=0;
-            ds.pop_back();
-        }
+// void f(vector<int> &nums,vector<int> &ds,vector<int>&mp,vector<vector<int>> &ans){
+//     if(ds.size()==nums.size()) {ans.push_back(ds);
+//     return ;}
+//     // if(ind=nums.size()) return ;
+//     for(int i=0;i<nums.size();i++){
+//         if(mp[i]=0){
+//             mp[i]=1;
+//             ds.push_back(nums[i]);
+//             f(nums,ds,mp,ans);
+//             mp[i]=0;
+//             ds.pop_back();
+//         }
         
+//     }
+// }
+void f(vector<int> &nums,vector<int> &ds,vector<int>&mp,vector<vector<int>> &ans,int ind){
+    if(ind==nums.size()) {
+        ans.push_back(ds);
+        return ;
     }
+    for(int i=ind;i<nums.size();i++){
+        swap(ds[ind],ds[i]);
+        f(nums,ds,mp,ans,ind+1);
+        swap(ds[ind],ds[i]);
+
+    }
+
 }
     vector<vector<int>> permute(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>ans;
-        vector<int> ds;
+        vector<int> ds=nums;
         vector<int>mp(n,0);
-        f(nums,ds,mp,ans);
+        f(nums,ds,mp,ans,0);
         return ans;
     }
 };
