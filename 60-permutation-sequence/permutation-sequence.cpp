29adf85b3c5b3f229f4149e1b1be9053f7496f0e
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-12 14:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (14.46%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -2,21 +2,26 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         int fact=1;
-        vector<int> number;
+        vector<int> num;
         for(int i=1;i<n;i++){
-            fact*=i;
-            number.push_back(i);
+            fact=fact*i;
+            num.push_back(i);
         }
-        number.push_back(n);
+            num.push_back(n);
+             k=k-1;
         string ans="";
-        k=k-1;
         while(true){
-            ans=ans+to_string(number[k/fact]);
-            number.erase(number.begin()+k/fact);
-            if(number.size()==0) break;
+            ans=ans+to_string(num[k/fact]);
+            num.erase(num.begin()+k/fact);
+            if(num.size()==0)break;
             k=k%fact;
-            fact=fact/number.size();
+            fact=fact/num.size();
+
+
+
+
         }
-        return ans;
+return ans;
+        
     }
 };
