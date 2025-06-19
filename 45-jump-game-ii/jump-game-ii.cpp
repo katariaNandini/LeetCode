f436d7abc196b5660e3ed6f3329bf304a227a75e
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-19 23:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.81%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-int f(vector<int>&nums,int ind,vector<int>&dp){
-    if(ind>=nums.size()-1) return 0;
-    // if(dp[ind]!=-1) return dp[ind];
-    int num=INT_MAX;
-    if(dp[ind]!=-1) return dp[ind];
-    int maxindex=min(ind+nums[ind],(int)(nums.size()-1));
-    for(int i=ind+1;i<=maxindex;i++){
-        int temp=f(nums,i,dp);  
-        if (temp != INT_MAX) {
-                num = min(num, 1 + temp);}
-    }
-    return dp[ind]=num;
-    
-}
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n,-1);
-        return f(nums,0,dp);
+        int n=nums.size(),l=0,r=0,maxjump=0,count=0;
+        // int r=0;
+        // int l=0;
+        // int maxjump=0;
+        while(r<n-1){
+            maxjump=0;
+            for(int i=l;i<=r;i++){
+                maxjump=max(maxjump,nums[i]+i);
+            
+            }
+            l=r+1;
+            r=maxjump;
+            count++;
+        }
+        return count;
     }
 };
