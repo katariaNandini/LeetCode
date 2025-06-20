a09b867cce55e1503baab75bdccf9218e233db22
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-20 23:48:54 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (92.08%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     int candy(vector<int>& rat) {
         int n=rat.size();
-        vector<int> left(n,1);
-        left[0]=1;
-        // right[n-1]=1;
-        int prev=1;
-        for(int i=1;i<n;i++){
-            if(rat[i]>rat[i-1])left[i]=left[i-1]+1;
-            // else left[i]=max(1,left[i-1]-1);
-                }
-                int sum=0;
-        sum+=left[n-1];
-        for(int i=n-2;i>=0;i--){
-            if (rat[i] > rat[i + 1]) {
-                    int cur=prev+1;
-                    sum+=max(left[i],cur);
-                    prev=cur;}
-            else{
-                prev=1;
-                sum+=left[i];
-
+        int sum=1;
+        int i=1;
+        while(i<n){
+            if(rat[i]==rat[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+            int uplen=0;
+            int peak=1;
+            while(i<n&&rat[i]>rat[i-1]){
+                peak++;
+                uplen++;
+                sum+=peak;
+                i++;
+            }
+            int downlen=0;
+            int down =1;
+            
+            while(i<n&&rat[i]<rat[i-1]){
+               
+                downlen++;
+                sum+=down;
+                 down++;
+                i++;
+            }
+            if(downlen>=peak)sum+=(downlen-peak+1);
         }
-        }                
-        // int count=0;
-        // for(int i=0;i<n;i++){
-        //     count+=max(left[i],right[i]);
-        // }
-return sum;
+        return sum;
     }
 };
