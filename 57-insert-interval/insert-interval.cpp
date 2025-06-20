3fa1b420e23b198eb82be1c69ff23a8a56f5de86
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-20 14:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (74.78%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& inte, vector<int>& newi) {
-        int n=inte.size();
-        vector<vector<int>> in;
-      in=inte;
-        int start=newi[0];
-        int end=newi[1];
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newi) {
+        int n=intervals.size();
+        vector<vector<int>> ans;
         int i=0;
-        while(i<in.size()){
-           int curStart = in[i][0];
-            int curEnd = in[i][1];
-
-            // If no overlap, move forward
-            if (end < curStart || start > curEnd) {
-                i++;
-            } else {
-                // Merge intervals
-                start = min(start, curStart);
-                end = max(end, curEnd);
-                in.erase(in.begin() + i); // erase and check this index again
-            }
+        while(i<n&&intervals[i][1]<newi[0]){
+            ans.push_back(intervals[i]);
+            i++;
         }
-        in.push_back({start,end});
-        sort(in.begin(),in.end());
-        return in;
-        
+        while(i<n&&intervals[i][0]<=newi[1]){
+            newi[0]=min(intervals[i][0],newi[0]);
+            newi[1]=max(intervals[i][1],newi[1]);
+            i++;
+        }
+        ans.push_back(newi);
+        while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+       return ans; 
     }
 };
