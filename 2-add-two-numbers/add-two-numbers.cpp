8c253d3a1453ea4ad8f5c0b755abc7b8adf72d97
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-06-23 19:37:55 +0530

    Time: 4 ms (21.90%) | Memory: 77.3 MB (12.17%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,28 +11,58 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy=new ListNode();
-        ListNode* temp=dummy;
-        int carry=0;
-        int sum=0;
-        while(l1!=NULL|| l2!=NULL || carry!=0){
-                    int sum=0;
+        ListNode* temp1=l1;
+        ListNode* temp2=l2;
+       
 
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            sum+=carry;
-            carry=sum/10;
-                    
-        ListNode* node=new ListNode(sum%10);
-        temp->next=node;
-        temp=temp->next;
+        ListNode* dummy=new ListNode(0);
+        ListNode* cur=dummy;
+        int remi =0;
+        while(temp1!=NULL&&temp2!=NULL){
+            int sum=temp1->val+temp2->val+remi;
+            //  remi=0;
+             ListNode * nexti=new ListNode(sum%10);
+            // if(sum>9)
+            // {nexti->val=sum
+             remi=sum/10;
+            // }
+            // else ListNode * nexti=new ListNode(sum);
+            // list->val= num%10;
+            cur->next=nexti;
+            // num=num/10;
+            // list=list->next;
+            cur=nexti;
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        while(temp1!=NULL){
+           int sum=temp1->val+remi;
+             ListNode * nexti=new ListNode(sum%10);
+             remi=sum/10;
+            cur->next=nexti;
+            cur=nexti;
+            temp1=temp1->next;
+             
 
+        }
+         while(temp2!=NULL){
+           int sum=temp2->val+remi;
+             ListNode * nexti=new ListNode(sum%10);
+             remi=sum/10;
+            cur->next=nexti;
+            cur=nexti;
+            temp2=temp2->next;
+             
+
+        }
+        if(remi!=0){
+
+            ListNode * nexti=new ListNode(remi);
+            //  remi=sum/10;
+            cur->next=nexti;
+            cur=nexti;
+            // temp2=temp2->next;
+        }
+        return dummy->next;
     }
-    return dummy->next;}
 };
