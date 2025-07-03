e1a38b4fb9be061b2213b6a96a2651220ff4f611
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-07-04 00:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (16.95%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,9 +4,15 @@ public:
         int n=nums.size();
         int maxi=INT_MIN;
         int sum=0;
+        // int start=-1,ansstart=-1,ansend=-1;to printing subarary or startindex and endindex of ans;
         for(int i=0;i<n;i++){
+            // if(sum==0) start=i;
             sum+=nums[i];
             maxi=max(sum,maxi);
+            // if(sum>maxi){
+            //     ansstart=start;
+            //     ansend=i;
+            // }
             if(sum<0)sum=0;
         }
         return maxi;
