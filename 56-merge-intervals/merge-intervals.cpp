f8832b816b9f366b5a1020b99484d519f9d57499
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-07-07 00:42:18 +0530

    Time: 11 ms (15.07%) | Memory: 24.4 MB (20.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-        if (intervals.empty()) return ans;
+                sort(intervals.begin(), intervals.end());
 
-        // Step 1: Sort intervals by starting time
-        sort(intervals.begin(), intervals.end());
-
-        int k=intervals.size();
-        int m=intervals[0].size();
         ans.push_back(intervals[0]);
-        for(int i=1;i<k;i++){
-                   int n=ans.size();
-                   if(ans[n-1][1]>=intervals[i][0]){
-                    int start=ans[n-1][0];
-                    int end =max(ans[n-1][1],intervals[i][1]);
-                    ans.pop_back();
-                    
-                    ans.push_back({start,end});
-                   }
-                   else         ans.push_back(intervals[i]);
-                   }
-                   return ans;
+        int n=intervals.size();
+        if(n==0||n==1)return intervals;
+        for(int i=1;i<n;i++){
+            int k=ans.size();
+            if(ans[k-1][1]>=intervals[i][0]){
+                int start=min(ans[k-1][0],intervals[i][0]);
+                int end=max(ans[k-1][1],intervals[i][1]);
+                ans.pop_back();
+                ans.push_back({start,end});
+            }
+            else ans.push_back(intervals[i]);
+        }
+        return ans;
     }
 };
