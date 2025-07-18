000e789a14c401b7540f62d5390f1ddac5e23b0e
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-07-18 19:06:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,63 +1,25 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
         int n=adj.size();
-        vector<int> visited(n,0);
-        queue<int>q;
-        visited[0]=1;
+       int vis[n]={0};
+        queue<int> q;
         q.push(0);
-        vector<int> bfs;
+        vis[0]=1;
+        vector<int> ans;
         while(!q.empty()){
             int node=q.front();
             q.pop();
-                    bfs.push_back(node);
-
+            ans.push_back(node);
             for(auto it:adj[node]){
-                if(visited[it]==0){
-                    visited[it]=1;
+                if(vis[it]==0){
+                    vis[it]=1;
                     q.push(it);
                 }
             }
         }
-        return bfs;
-        // Code here
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        // Now using vector of vectors instead of array of vectors
-        vector<vector<int>> adj(V);
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u); // For undirected graph, add both u->v and v->u
-        }
-
-        Solution obj;
-        vector<int> ans = obj.bfsOfGraph(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
