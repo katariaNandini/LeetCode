3d824893c2ab61857c9ad946d41d29d519646b91
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-07-19 16:00:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (20.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-             if(grid.empty()) return 0;
-
-     int n=grid.size();
-     int m=grid[0].size(); 
-vector<vector<int>> vis(n, vector<int>(m, 0));
-     queue<pair<pair<int,int>,int>>q;
-     int cntfr=0;
-
-     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j]==2){
-                vis[i][j]=2;
-                q.push({{i,j},0});
-            }
-            else vis[i][j]=0;
-            if(grid[i][j]==1) cntfr++;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){vis[i][j]=2;
+                q.push({{i,j},0});}
+                else vis[i][j]=0;
+            
         }}
- int rw[] = {-1, 0, +1, 0};  // Row movement for 4 directions (up, right, down, left)
-        int cw[] = {0, 1, 0, -1};
         int tm=0;
-        int cnt=0;
+        int rtra[4]={-1,1,0,0};
+        int ctra[4]={0,0,-1,1};
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
             int t=q.front().second;
             q.pop();
-                    tm=max(t,tm);
-
+            tm=max(tm,t);
             for(int i=0;i<4;i++){
-                int nr=r+rw[i];
-                int nc=c+cw[i];
-                if(nr<n && nr>=0 && nc<m &&nc>=0 && vis[nr][nc]==0 && grid[nr][nc]==1){
+                int nr=r+rtra[i];
+                int nc=c+ctra[i];
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&vis[nr][nc]==0&&grid[nr][nc]==1){
                     q.push({{nr,nc},t+1});
                     vis[nr][nc]=2;
-                    cnt++;
-                } 
+
+                }
             }
 
         }
-        if(cnt!=cntfr) return -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1&&vis[i][j]!=2)return -1;
+            }
+        }
         return tm;
-     }
-    
+    }
 };
