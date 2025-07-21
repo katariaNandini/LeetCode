c450783c046d0b5369a88be594efc181e6e17786
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-07-21 13:25:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,32 +1,36 @@
 class Solution {
   public:
-  bool dfs(int node,int parent,vector<vector<int>>&adj,vector<int>vis,vector<int>&pathvis){
-      vis[node]=1;
-      pathvis[node]=1;
-      for(auto it:adj[node]){
-          if(it!=parent&&vis[it]==0){
-              if(dfs(it,node,adj,vis,pathvis)==true)return true;
-          }
-          else if(pathvis[it])return  true;
-      }
-      pathvis[node]=0;
-      return false;
-  }
     bool isCyclic(int V, vector<vector<int>> &edges) {
-        vector<int> vis(V,0);
-        vector<int> pathvis(V,0);
         // code here
-        vector<vector<int>>adj(V);
-        for(auto it:edges){
+                vector<vector<int>> adj(V);
+        for(auto it :edges){
             int u=it[0];
             int v=it[1];
             adj[u].push_back(v);
         }
+        vector<int>indegree(V,0);
         for(int i=0;i<V;i++){
-            if(vis[i]==0)
-            if(dfs(i,-1,adj,vis,pathvis
-            )==true)return true;
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
         }
-        return false;
+        stack<int>st;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)st.push(i);
+            
+        }
+        vector<int> ans;
+        while(!st.empty()){
+            int node=st.top();
+            st.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)st.push(it);
+            }
+        }
+        
+        if(ans.size()==V)return false;
+        else return true;
     }
 };
