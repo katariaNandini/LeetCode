981d7200c47270578902d05790025a2506cd2841
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-08-14 21:43:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,76 +1,34 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int kthElement(vector<int>& a, vector<int>& b, int k) {
-        int n1=a.size();
-        int n2=b.size();
-                if (k > n1 + n2) return -1;
-        if (n1 > n2) return kthElement(b, a, k);
-
-       // int half=k/2;
-     int low = max(0, k - n2);
-int high = min(k, n1);
-        //int totalh=(n1+n2+1)/2;
-        //int ansindex=-1;
+    int kthElement(vector<int> &nums1, vector<int> &nums2, int k) {
+        // code here
+         int n=nums1.size();
+        int m=nums2.size();
+        if(n>m)return kthElement(nums2,nums1,k);
+       int low = max(0, k - m);
+int high = min(k, n);
+        int left=k;
         while(low<=high){
-            int mid1=(high+low)/2;
-            int mid2=k-mid1;
+            int mid1=(low+high)/2;
             int l1=INT_MIN;
             int l2=INT_MIN;
             int r1=INT_MAX;
             int r2=INT_MAX;
+            int mid2=left-mid1;
+            if(mid1<n)r1=nums1[mid1];
+            if(mid2<m)r2=nums2[mid2];
+            if(mid1-1>=0)l1=nums1[mid1-1];
+            if(mid2-1>=0)l2=nums2[mid2-1];
+            if(l1<=r2&&l2<=r1){
+                // if((n+m)%2==0)return (double)(min(r1,r2)+max(l1,l2))/2.00;
+                // else
+                return max(l1,l2);
+                }
+            else if(l1>r2)high=mid1-1;
+            else low=mid1+1;
 
-            if(mid1 >0) l1=a[mid1-1];
-            if(mid2 >0) l2=b[mid2-1];
-            if(mid1<n1) r1=a[mid1];
-            if(mid2<n2) r2=b[mid2];
-            if(l1<=r2&&l2<=r1) {
-            return max(l1,l2);
-               // return (double(max(l1,l2)+min(r1,r2)))/2.0;
-            }
-           else  if(l1>r2) high=mid1-1;
-             else low=mid1+1;
-        }
-return -1;
-        // code here
+            
+                    }
+        return 0;
     }
 };
-
-//{ Driver Code Starts.
-
-// Driver code
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, m, k;
-        cin >> k;
-        cin.ignore();
-        string input;
-        int num;
-        vector<int> a, b;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            a.push_back(num);
-        }
-
-        getline(cin, input);
-        stringstream s3(input);
-        while (s3 >> num) {
-            b.push_back(num);
-        }
-
-        Solution ob;
-        cout << ob.kthElement(a, b, k) << endl << "~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
