c2811c0767ef21a868c9b8b6c96eaa2c6a139de2
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-08-26 01:04:26 +0530

    Time: 31 ms (16.40%) | Memory: 38.6 MB (36.51%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -2,10 +2,11 @@ typedef pair<int,int> pi;
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pi,vector<pi>,greater<pi>>pq;
+        priority_queue<pi>pq;
         int n=arr.size();
         for(int i=0;i<n;i++){
             pq.push({abs(arr[i]-x),i});
+            if(pq.size()>k)pq.pop();
         }
         vector<int> ans;
         for(int i=0;i<k;i++){
