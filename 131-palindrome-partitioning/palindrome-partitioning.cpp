9e4823938b5f05db9348c0d8de8164927628c398
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-09-14 00:01:21 +0530

    Time: 67 ms (23.53%) | Memory: 61.7 MB (48.38%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,30 +1,32 @@
+// notes in yellow notebook
+
 class Solution {
 public:
-bool ispalidrom(string s,int start,int end){
-    while(start<end){
+bool pali(string s ,int start,int end){
+    while(start<=end){
         if(s[start]!=s[end])return false;
         start++;
         end--;
     }
     return true;
 }
-
-void solve(int ind,string s,vector<string>&ds,vector<vector<string>>&ans){
-    if(ind==s.size()){ans.push_back(ds);
-     return ;}
-     for(int i=ind;i<s.size();i++){
-        if(ispalidrom(s,ind,i)){
-            ds.push_back(s.substr(ind,i-ind+1));
-            solve(i+1,s,ds,ans);
-            ds.pop_back();
+void f(int i,string s, vector<vector<string>> &ans,vector<string>&path){
+    if(i==s.size()){
+        ans.push_back(path);
+        return ;
+    }
+    for(int a=i;a<s.size();a++){
+        if(pali(s,i,a)){
+            path.push_back(s.substr(i,a-i+1));
+            f(a+1,s,ans,path);
+            path.pop_back();
         }
-     }
-
+    }
 }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> ds;
-        solve(0,s,ds,ans);
+        vector<string>path;
+        f(0,s,ans,path);
         return ans;
     }
 };
