f8d5b18c7fa3ce7be713334d7c5e4ef5e8fd7e8b
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-09-18 17:14:38 +0530

    Time: 1878 ms (5.00%) | Memory: 429.7 MB (9.51%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-bool ispali(int i,int j,string s){
-    while(i<j){
-        if(s[i]==s[j]){
-            i++;
-            j--;
-        }
-        else return false;
-    }
-    return true;
+bool ispali(int i,int j,string s,vector<vector<int>> &dp){
+    if(i>j)return true;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(s[i]==s[j]) {
+        return dp[i][j]=ispali(i+1,j-1,s,dp);}
+    return dp[i][j]= false;
 }
     int countSubstrings(string s) {
+       
      int n=s.size();
+      vector<vector<int>>dp(n,vector<int>(n,-1));
      int count=0;
      for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
-            if(ispali(i,j,s))count++;
+            if(ispali(i,j,s,dp))count++;
         }
-     }  
+     }
+      
      return count; 
     }
 };
