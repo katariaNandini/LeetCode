b6cb509f281e06f9d89d030665cfa046e1ce653c
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-09-20 02:47:43 +0530

    Time: 16 ms (96.08%) | Memory: 70.9 MB (88.27%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n==1||n==0)return false;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]==nums[i])return true;
+        }
+        return false;
+    }
+};
