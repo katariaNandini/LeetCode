c67d7fbae5170edc8ee279277d6d871b8ff885ea
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-09-22 04:08:01 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (59.29%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int sum=0;
+        int count1=0;
+        int i=0;
+        int j=0;
         int n=nums.size();
-        int sum=0,i=0,j=0,count1=0;
-        if (goal < 0) return 0;
         while(j<n){
             sum+=nums[j];
             while(sum>goal){
@@ -13,16 +15,20 @@ public:
             count1+=j-i+1;
             j++;
         }
-        int i2 = 0, j2 = 0, sum2 = 0, count2 = 0;
+        i=0;
+        j=0;
+        int count2=0;
+        sum=0;
         if (goal == 0) return count1;
-    while(j2<n){
-            sum2+=nums[j2];
-            while(sum2>goal-1){
-                sum2-=nums[i2];
-                i2++;
+
+        while(j<n){
+             sum+=nums[j];
+            while(sum>goal-1){
+                sum-=nums[i];
+                i++;
             }
-            count2+=j2-i2+1;
-            j2++;
+            count2+=j-i+1;
+            j++;
         }
         return count1-count2;
     }
