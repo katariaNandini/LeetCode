a03d91e285d2a8f34ffae62e16e7d5567ead1bf3
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-09-22 03:16:13 +0530

    Time: 1869 ms (5.13%) | Memory: 74.9 MB (73.98%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> mp;
+    int totalFruit(vector<int>& f) {
+        unordered_map<int,int> mp;
         int i=0;
         int j=0;
-         int n=fruits.size();
-         int count=0;
-         int maxi=0;
-         while(j<n){
-            mp[fruits[j]]=j;
-            if(mp.size()>2){
-                            int minIndex = INT_MAX;
-                int fruitToRemove = -1;
-                for (auto& [fruit, index] : mp) {
-                    if (index < minIndex) {
-                        minIndex = index;
-                        fruitToRemove = fruit;
-                    }
-                }
-
-                mp.erase(fruitToRemove);
-                i = minIndex + 1;
-
-                  } 
-              
-
-            
-          count=j-i+1;
-          maxi=max(count,maxi);
-          j++;
-         }
-return maxi;
+        int n=f.size();
+        int maxi=0;
+        while(j<n){
+             mp[f[j]]++;
+            while(mp.size()>2){
+                mp[f[i]]--;
+                if(mp[f[i]]==0)mp.erase(f[i]);
+                i++;
+            }
+            j=max(j,i);
+           
+            cout<<i<<j<<endl;
+            maxi=max(j-i+1,maxi);
+            j++;
+        }
+        return maxi;
     }
 };
-
