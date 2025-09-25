db6c8a2a51eed5ab9220721e70312937ef272f9f
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2025-09-26 01:36:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -1,41 +1,25 @@
-// User function template for C++
-
 class Solution {
   public:
     int longestKSubstr(string &s, int k) {
-        // your code here
-        unordered_map<char, int> mp;
+        // code here
+        map<int,int> mp;
         int i=0;
         int j=0;
-         int n=s.size();
-         int count=-1;
-         int maxi=-1;
-         while(j<n){
-            mp[s[j]]=j;
-            if(mp.size()>k){
-                            int minIndex = INT_MAX;
-char fruitToRemove = '\0';
-                for (auto it = mp.begin(); it != mp.end(); ++it) {
-    char fruit = it->first;
-    int index = it->second;
-                    if (index < minIndex) {
-                        minIndex = index;
-                        fruitToRemove = fruit;
-                    }
+        int n=s.size();
+        int count=0;
+        while(j<n){
+            mp[s[j]]++;
+            while(mp.size()>k){
+                mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
                 }
-
-                mp.erase(fruitToRemove);
-                i = minIndex + 1;
-
-                  } 
-              
-
-            
-          if (mp.size() == k) {
-                maxi = max(maxi, j - i + 1);
-            }
-          j++;
-         }
-return maxi;
+                i++;
+                }
+            if(mp.size()==k)count=max(count,j-i+1);
+            j++;
+        }
+        if(count==0)return -1;
+        return count;
     }
 };
