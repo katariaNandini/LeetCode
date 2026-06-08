86cca3a22c993284b063ca6f4bfaa71ced720565
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-06-08 22:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (50.91%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
-        int maxi=INT_MIN;
         int sum=0;
-        // int start=-1,ansstart=-1,ansend=-1;to printing subarary or startindex and endindex of ans;
+        int maxi=INT_MIN;
+
+        int last=nums[0];
         for(int i=0;i<n;i++){
-            // if(sum==0) start=i;
             sum+=nums[i];
             maxi=max(sum,maxi);
-            // if(sum>maxi){
-            //     ansstart=start;
-            //     ansend=i;
-            // }
-            if(sum<0)sum=0;
+            if(sum<=0){
+              sum=0;  
+            }
+
         }
         return maxi;
     }
