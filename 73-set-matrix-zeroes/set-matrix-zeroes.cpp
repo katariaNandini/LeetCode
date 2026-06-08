daa57a2df5f091187ad18f85ba3392f1991af629
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-06-08 21:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (75.75%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0=1;
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<int> row(n,-1);
+        vector<int> col(m,-1);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    if(j!=0)matrix[0][j]=0;
-                    else col0=0;
-                }
+                if(matrix[i][j]==0) {row[i]=0;
+                col[j]=0;}               
             }
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]!=0){
-                if(matrix[i][0]==0||matrix[0][j]==0) matrix[i][j]=0;
-}
-
-  
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==0||col[j]==0)matrix[i][j]=0;
             }
         }
-         if (matrix[0][0] == 0) {
-            for (int j = 0; j < m; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        // Step 4: Zero out first column if needed
-        if (col0 == 0) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-        // return matrix
     }
 };
