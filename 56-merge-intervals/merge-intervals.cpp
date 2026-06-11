730ca86df887730736fa84a04fb6f5bc234e77ad
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-06-11 21:06:16 +0530

    Time: 7 ms (40.07%) | Memory: 23.9 MB (31.45%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,9 +6,9 @@ public:
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
         for(int i=1;i<n;i++){
-           int end=ans.size();
-            if(ans[end-1][1]>=intervals[i][0]){
-                ans[end-1][1]=max(intervals[i][1],ans[end-1][1]);
+           
+            if(ans.back()[1]>=intervals[i][0]){
+                ans.back()[1]=max(intervals[i][1],ans.back()[1]);
             }
             else{
                
