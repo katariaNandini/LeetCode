c87b7e81f68d08ef7971f967c78cfbbb904cfafd
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-06-11 22:30:19 +0530

    Time: 2 ms (31.72%) | Memory: 97.3 MB (59.80%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         // if you are selling on ith day you can 
-        // buy on the minimun price from 11st day to (i-1) day
-        int minprice=prices[0];
-        int profit=0;
-        int maxprofit=0;
+        int  min=prices[0];
+        int maxi=0;
         int n=prices.size();
-        for(int i=1;i<n;i++){
-            minprice=min(minprice,prices[i]);
-            profit=prices[i]-minprice;
-            maxprofit=max(maxprofit,profit);
+        for(int i=0;i<n;i++){
+             if(min>prices[i])min=prices[i];
+             else{
+                int prof=prices[i]-min;
+                maxi=max(maxi,prof);
+             }
         }
-        return maxprofit;
+        return maxi;
     }
 };
