f81cb52998091251c4cc2db07c1000179243f0a0
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-06-24 21:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.57%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-int robber(vector<int> & nums,int ind,vector<int>&dp){
-    if(ind==0)return nums[ind];
-    if(ind==1) return max(nums[1],nums[0]);
-    if(dp[ind]!=-1) return dp[ind];
-    int taken=INT_MIN;
-    if(ind>1)
-     taken= nums[ind]+robber(nums,ind-2,dp);
-    int nottaken=robber(nums,ind-1,dp);
-    return dp[ind]=max(taken,nottaken);
+int robb(vector<int> & nums,int i,vector<int>& dp){
+    if(i<0) return 0;
+    if(i==0) return dp[i]=nums[i];
+    if(dp[i]!=-1)return dp[i];
+    int take=nums[i]+robb(nums,i-2,dp);
+    int nottake=robb(nums,i-1,dp);
+    return dp[i]=max(take,nottake);
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,0);
-        dp[0]=nums[0];
-        if(n>=2)
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            int taken=dp[i-2]+nums[i];
-            int nottaken=dp[i-1];
-            int cur=max(taken,nottaken);
-            dp[i]=cur;
-        }
-        return dp[n-1];
+        vector<int> dp(n,-1);
+        return robb(nums,n-1,dp);
     }
 };
