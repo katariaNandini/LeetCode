ca6827c2797b908f36f50fee2ac6bb0ec23375b0
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-07-17 22:02:47 +0530

    Time: 652 ms (5.04%) | Memory: 295.4 MB (30.40%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-int f(int ind,int n,int prev,vector<int>& nums,vector<vector<int>> & dp){
-    if(ind==n) return 0;
-if(dp[ind][prev+1]!=-1) return dp[ind][prev+1]; 
-    // take conditon
-    int take = 0;
-        if(prev == -1 || nums[ind] > nums[prev]) {
-            take = 1 + f(ind + 1, n,ind, nums, dp);
-        }
-      
+int recur(vector<int>& nums,int i,int j,vector<vector<int>>& dp){
     
-    return dp[ind][prev+1]=max(f(ind+1,n,prev,nums,dp),take); 
+    if(i==nums.size())return 0;
+    if(dp[i][j+1]!=-1)return dp[i][j+1];
 
+    int take=0;
+    if(j==-1||nums[j]<nums[i])take=1+recur(nums,i+1,i,dp);
+    int nottake=recur(nums,i+1,j,dp);
+    return dp[i][j+1]=max(nottake,take);
 }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        vector<int> previ(n+1,0),cur(n+1,0);
-    for(int ind=n-1;ind>=0;ind--){
-        for(int prev=ind-1;prev>=-1;prev--){
-            // take conditon
-                int take = 0;
-                    if( prev == -1 ||nums[ind] > nums[prev]) {
-                        take = 1 +cur[ind+1];
-                    }
-                
-                
-                previ[prev+1]=max(cur[prev+1] ,take); 
-                    }
-         cur=previ;           
-    }
-        return cur[0];
-        // return f(0,n,-1,nums,dp);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        return recur(nums,0,-1,dp);
     }
 };
