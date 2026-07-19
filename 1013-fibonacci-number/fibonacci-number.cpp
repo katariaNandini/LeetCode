2f0b4d125557f97f44ac3e25d5b00d9925ea1c93
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-07-19 14:14:33 +0530

    Time: 11 ms (37.99%) | Memory: 7.7 MB (65.34%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-int fibonacci(int n,vector<int>& dp){
-    if(n<=1)return n;
-    if(dp[n]!=-1) return dp[n];
-    return dp[n]=fibonacci(n-1,dp)+fibonacci(n-2,dp);
+int recur(int n){
+if(n==0) return 0;
+     if(n==1) return 1;
+
+        return recur(n-1)+recur(n-2); 
 }
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return fibonacci(n,dp);
+       return recur(n);
     }
 };
