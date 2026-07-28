ed7f1d9e346ca87710b9d13a4c3036db82e85668
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-07-28 21:52:46 +0530

    Time: 66 ms (8.00%) | Memory: 82.8 MB (8.72%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-int f(int i,int j,string s1,string s2){
-    if(i<0) return j+1;
-    if(j<0) return i+1;
-    if(s1[i]==s2[j])return f(i-1,j-1,s1,s2);
-    return min((1+f(i,j-1,s1,s2)),min((1+f(i-1,j,s1,s2)),(1+f(i-1,j-1,s1,s2))));
-}
+     int recur(string w1,string w2,int l1,int l2,vector<vector<int>> &dp){
+
+        if(l1==w1.length()|| l2==w2.length()){
+            if(l1==w1.size()){
+                return w2.size()-l2;
+            }
+            else return w1.size()-l1;
+        }
+
+        if(dp[l1][l2]!=-1) return dp[l1][l2];
+        if(w1[l1]==w2[l2]) {
+        return dp[l1][l2]= recur(w1,w2,l1+1,l2+1,dp);
+        }
+        int insert=1+recur(w1,w2,l1,l2+1,dp);
+        int delele=1+recur(w1,w2,l1+1,l2,dp);
+        int replace=1+recur(w1,w2,l1+1,l2+1,dp);
+        return dp[l1][l2]= min(insert,min(delele,replace));
+     }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        vector<int> prev(m+1,0),cur(m+1,0);
-        for(int i=0;i<=m;i++) prev[i]=i;
-        // for(int i=0;i<=n;i++)dp[i][0]=i;
-        for(int i=1;i<=n;i++){
-            cur[0] = i;
-            for(int j=1;j<=m;j++){
-                 if(word1[i-1]==word2[j-1])cur[j] =prev[j-1];
-                 else cur[j]= min((1+prev[j]),min((1+prev[j-1]),(1+cur[j-1])));   
-            }
-            prev=cur;
-        }
-    return   prev[m];
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return recur(word1,word2,0,0,dp);
     }
 };
