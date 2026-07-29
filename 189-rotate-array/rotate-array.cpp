4a1b5b6b2c0b67e1dca27d6c8a1c62c783ad96b4
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-07-29 21:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (70.59%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,44 +1,10 @@
 class Solution {
 public:
-    void rotate(vector<int>& arr, int k) {
-        
-        int n=arr.size();
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
         k=k%n;
-        vector<int> temp(k+1);
-        int l=0;
-        for(int i=n-k;i<n;i++){
-            temp[l]=arr[i];
-            l++;
-        }
-        for(int i=n-k-1;i>=0;i--){
-          arr[i+k]=arr[i];
-        }
-        for(int i=0;i<k;i++){
-            arr[i]=temp[i];
-        }
+        reverse(nums.begin(),nums.begin()+n-k);
+        reverse(nums.begin()+n-k,nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
-
-
-// class Solution {
-// public:
-//     void rotate(vector<int>& nums, int k) {
-        
-//         int n=nums.size();
-//         vector<int> ans(n);
-//         k=k%n;
-//         int l=0;
-//         for(int i=n-k;i<n;i++){
-//             ans[l]=nums[i];
-//             l++;
-//         }
-//         for(int i=0;i<n-k;i++){
-//             ans[l]=nums[i];
-//             l++;
-//         }
-// nums=ans;
-
-//     }
-// };
-
-
