cba42e0d982b846998fc0ee024167f5ce396a1b9
Nandini Kataria <164673213+katariaNandini@users.noreply.github.com>
2026-07-29 21:07:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (70.89%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-// Intuition: By observation, we see that the first column of the original matrix is the reverse of the first row of the rotated matrix, so that’s why we transpose the matrix and then reverse each row, and since we are making changes in the matrix itself space complexity gets reduced to O(1).
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
+        int m=matrix[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                swap(matrix[i][j],matrix[j][i]);
+                if(i!=j){
+                    int temp=matrix[i][j];
+                  matrix[i][j]=matrix[j][i];
+                  matrix[j][i]=temp;
+                }
             }
+
         }
         for(int i=0;i<n;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
+        
     }
 };
